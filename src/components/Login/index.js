bb6020740c91ba717b0cfba24919cd95d99ea0d7
zanaa <mnaranzana@gmail.com>
2021-03-26 00:15:23 +0100

    second commit

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -18,7 +18,7 @@ const Login = (props) => {
             <Form.Label>Email address</Form.Label>
             <Form.Control type="email" placeholder="Enter email" />
             <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
+              We'll never share your email with anyone else!
             </Form.Text>
           </Form.Group>
 
